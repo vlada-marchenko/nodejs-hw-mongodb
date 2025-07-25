0d5db579cf8ff54ccf36b7ecd4efa40e19d23e1f
vlada-marchenko <vvvvadya44@gmail.com>
2025-07-26 01:36:56 +0200

    third

--- src/server.js
+++ src/server.js
@@ -20,6 +20,10 @@ app.use(pino({
 }));
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Server is working!');
+});
+
 app.all('/{*splat}', (req, res, next) => {
     res.status(404).json({
         message: 'Not found'
