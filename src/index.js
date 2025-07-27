a308976c329fa6177dd055019039955c08d60d2f
vlada-marchenko <vvvvadya44@gmail.com>
2025-07-27 14:04:30 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
-
-import mongoose from "mongoose";
 import { setUpServer } from "./server.js";
 import { initMongoConnection } from "./db/initMongoConnection.js";
-import { ContactCollection } from "./db/models/contact.js";
 
 const bootstrap = async () => {
-  await initMongoConnection();
-  console.log("Mongo connected to DB:", mongoose.connection.name);
-
-  const count = await ContactCollection.estimatedDocumentCount();
-  const first = await ContactCollection.findOne().lean();
-  console.log("contacts count:", count);
-  console.log("first contact sample:", first);
-
-  setUpServer();
+    await initMongoConnection();
+    setUpServer();
 };
 
 bootstrap();
